72118b03d37d9d9adcec05a4646df1b77fb14931
Chris Fordham <chris@fordham.id.au>
2023-08-29 14:38:24 +1000

    Enable tcp forwarding.

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -2,6 +2,9 @@
 
 echo "$SSH_PUBLIC_KEY" > /root/.ssh/authorized_keys
 
+# enable tcp forward for ssh tunnels
+sed -i 's/AllowTcpForwarding no/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+
 ssh-keygen -A
 
 echo "> $@"
